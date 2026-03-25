2076cd5a4a1663c0bec3548c45193bde672efeee
Rida Hlou <rhlou@e1r5p8.1337.ma>
2026-03-25 10:18:36 +0100

    A-MAZE-ING

--- src/generator.py
+++ src/generator.py
@@ -41,7 +41,6 @@ class MazeGenerator:
             2D list of ints representing the maze wall bitmasks.
         """
         random.seed(self.seed)
-        random.seed(self.seed)
         self._carve_dfs()
         self._place_42()
         self._enforce_borders()
